97f6c79f83d6a2daa5352d2a9c7af4283a1124c9
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-06 14:35:15 +0530

    Intercepting packets -- creating proxy

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,2 +1,13 @@
 #!usr/bin/env/python
 
+import netfilterqueue
+
+def process_packet(packet):
+    print(packet)
+    packet.drop()  # This will cut the connection of internet for the client
+    #packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
