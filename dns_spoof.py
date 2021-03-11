ea6ac0158d4791f168836ccba35c901584a56562
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-11 12:23:40 +0530

    Converted payload to scapy packet

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,9 +1,11 @@
 #!usr/bin/env/python
 
 import netfilterqueue
+import scapy.all as scapy
 
 def process_packet(packet):
-    print(packet)
+    scapy_packet = scapy.IP(packet.get_payload()) # This will convert the packet payload to scapy packet
+    print(scapy_packet)
     packet.drop()  # This will cut the connection of internet for the client
     #packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
 
